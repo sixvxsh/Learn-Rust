3868a128a30972714e501215c1f8c291b2d979f2
sixvxsh <sixvxsh.j@gmail.com>
2023-06-05 12:40:44 +0330

    init

---
+++ section_2/target/debug/deps/section_2-b53edd27548b0321.d
@@ -0,0 +1,5 @@
+/home/sixvxsh/Programming/rust/rust-tutorial/section_2/target/debug/deps/section_2-b53edd27548b0321.rmeta: src/main.rs
+
+/home/sixvxsh/Programming/rust/rust-tutorial/section_2/target/debug/deps/section_2-b53edd27548b0321.d: src/main.rs
+
+src/main.rs:
